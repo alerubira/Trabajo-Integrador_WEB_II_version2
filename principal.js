e69007b7f3be126bc1651b73720ab74db9829ec8
alerubira <100098255+alerubira@users.noreply.github.com>
2024-04-02 12:38:13 -0300

    Update principal.js

--- principal.js
+++ principal.js
@@ -5,28 +5,13 @@ fetch ('https://randomuser.me/api/')
    .then(response=>response.json())
    .then(data=>{
        console.log(data);
-       
-      /*for(let p of data){
-        let personaje={
-          nombre:p.name,
-          vive:p.alive,
-          imagen:p.image,
-          casa:p.house,
-          raza:p.species,
-          genero:p.gender
-        }
-        if(!p.house){
-           personaje.casa="Sin Casa";
-        }
-        personajes.push(personaje);
-      }
-   
-      console.log(personajes);*/
+      
       })
     .catch(error=> console.error('Error al acceder a la pagina ', error));
     console.log(`${cartel}`);
     let parrafo=document.createElement("p");
     parrafo.textContent=`${cartel}`;
     divCabera.appendChild(parrafo);
-    
+    console.log()
+    if(true){};
     
